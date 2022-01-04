5fce21feda30fe5258a9ae8d054b2c381629eeb1
keegankb93 <80284046+keegankb93@users.noreply.github.com>
2022-01-04 16:03:37 -0600

    long column names

--- src/api/test-collection-relation/controllers/test-collection-relation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  test-collection-relation controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::test-collection-relation.test-collection-relation');
--- src/api/test-collection-relation/services/test-collection-relation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-collection-relation service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::test-collection-relation.test-collection-relation');
